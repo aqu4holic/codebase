26575a40b8b2657a2c9150dd953ce3047ab74673
BlackWhite11245 <54235829+BlackWhite11245@users.noreply.github.com>
2021-01-23 22:24:48 +0700

    Add files via upload

---
+++ core.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int oo=INT_MAX;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> ii; 
+#define X first
+#define Y second
+#define pb push_back
+#define ins insert
+#define el '\n'
+
+#define Fast ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+#define TEN " "
+#define Fio freopen (TEN".inp","r",stdin);freopen (TEN".out","w",stdout);
+#define Io freopen (TEN".inp","r",stdin);
+#define clrscr system("cls");
+
+
+
+int main()
+{
+	Fast
+	// Fio
+	// Io
+	//
+	//clock_t tStart = clock();
+	
+	
+
+	//printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+	return 0;
+}
