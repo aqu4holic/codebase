ef7ca0540baba7f714d31b078fb78d4e40e07410
BlackWhite <gautruc.anh1234@gmail.com>
2021-07-22 15:08:23 +0700

    Updated debug and time function

--- core.cpp
+++ core.cpp
@@ -2,7 +2,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define debugg(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#define time__(d) \
+    for ( \
+        auto blockTime = make_pair(chrono::high_resolution_clock::now(), true); \
+        blockTime.second; \
+        debugg("%s: %d ms\n", d, (int)chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - blockTime.first).count()), blockTime.second = false \
+    )
 
 #define int long long
 #define FOR(i, a, b) for (int i = a; i <= b; ++i)
@@ -46,8 +79,6 @@ signed main()
 	// Fio
 	// Ro
 	// Wo
-	//
-	// auto begin = std::chrono::high_resolution_clock::now();
 
 	int test_case = 1;
     // cin >> test_case;
@@ -58,7 +89,5 @@ signed main()
         // cout << "Case #" << tt << ": " <<  << el;
     }
 
-	// auto end = std::chrono::high_resolution_clock::now();
-	// cout << setprecision(5) << fixed << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
 	return 0;
 }
