2f5e0ab0a3fccdea4160cf5d44b874d2192b1905
BlackWhite <gautruc.anh1234@gmail.com>
2021-09-20 23:35:59 +0700

    Minor changes

--- core.cpp
+++ core.cpp
@@ -15,7 +15,6 @@ void __print(char x) {cerr << '\'' << x << '\'';}
 void __print(const char *x) {cerr << '\"' << x << '\"';}
 void __print(const string &x) {cerr << '\"' << x << '\"';}
 void __print(bool x) {cerr << (x ? "true" : "false");}
-
 template<typename T, typename V>
 void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
 template<typename T>
